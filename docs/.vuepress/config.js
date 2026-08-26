04bf8222312c84f11eb695efcf3519bcebc09d6c
Daniel Chemko <dchemko@gmail.com>
2026-08-26 15:11:47 -0700

    Fixed up from starter

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,6 +3,7 @@ import { defaultTheme } from '@vuepress/theme-default'
 import { defineUserConfig } from 'vuepress'
 
 export default defineUserConfig({
+    base: "/homebox-docs/",
     bundler: viteBundler(),
     theme: defaultTheme(),
 })
